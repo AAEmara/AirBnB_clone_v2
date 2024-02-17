00831dd30e041e099a6b4841f6ed87c4e8e096f5
AAEmara <abdelrahmanemara18@gmail.com>
2024-02-17 00:56:37 -0800

    0x04: Task0 Flask Python Script - 1st Fix.
    Added a code block to make the web app
    run on `0.0.0.0` on port `5000`.
    Also documented and changed the function's
    name.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
+def hello():
+    """Returning a greeting message for visiting the `/`."""
     return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

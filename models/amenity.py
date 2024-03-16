1939a38a015a398291fca6d9f517f2e8cb27aeeb
AAEmara <abdelrahmanemara18@gmail.com>
2024-03-16 00:56:50 -0700

    V2: Task10 Python Scripts - Amenity M:M.
    Added the many to many relationship
    between the Amenity Table and the Place
    Table through using an association table.

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Creates an Amenity Instance."""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializing an Amenity Instance."""
+        super().__init__(*args, **kwargs)
--- models/place.py
+++ models/place.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from models.amenity import Amenity
+from sqlalchemy import Table, Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -20,9 +21,21 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        # Deining relationships with the `places` table.
         user = relationship('User', back_populates='places')
         cities = relationship('City', back_populates='places')
         reviews = relationship('Review', back_populates='places')
+        # Creating an association table.
+        place_amenity = Table('place_amenity', Base.metadata,
+                              Column('place_id', String(60),
+                                     ForeignKey('places.id'),
+                                     primary_key=True, nullable=False),
+                              Column('amenity_id', String(60),
+                                     ForeignKey('amenities.id'),
+                                     primary_key=True, nullable=False))
+        # Creating the many to many relationship between places and amenities.
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -44,9 +57,20 @@ class Place(BaseModel, Base):
         @property
         def reviews(self):
             """ Returns a list of Review instances for a certain place_id. """
-            place_objs = storage.all()
+            place_objs = storage.all(Place)
             review_list = list()
             for key, obj in place_objs.items():
                 if obj['place_id'] == self.id:
                     review_list.append(obj)
             return (review_list)
+
+        @property
+        def amenities(self):
+            """Returns the list of IDs for the Amenity Class."""
+            return (amenity_ids)
+
+        @amenities.setter
+        def amenities(self, inserted_obj):
+            """Handling the append mehthod, for appending amenities."""
+            if isinstance(inserted_obj, Amenity):
+                self.amenity_ids.append(inserted_obj.id)
